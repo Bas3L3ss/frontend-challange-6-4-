70236e36abc9ad4ee2e82138973e9a5f77f3e4e5
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-09-04 23:15:14 +0700

    commit

--- src/components/ChoiceButton.tsx
+++ src/components/ChoiceButton.tsx
@@ -18,13 +18,17 @@ function ChoiceButton({
   currentChoice,
   handleClick,
 }: ChoiceButtonProps) {
+  const isActive = choiceName === currentChoice;
+
   return (
     <button
       id={`choiceButton-${choiceName}`}
       className={`${
-        choiceName === currentChoice ? "bg-black" : `${color}`
-      }  md:p-4 p-3 absolute rounded-full shadow-lg  `}
+        isActive ? "bg-black" : `${color}`
+      }  md:p-4 p-3 absolute rounded-full shadow-lg`}
       onClick={() => handleClick(choiceName, color)}
+      aria-pressed={isActive} // Indicates the button's active state
+      aria-label={`Choose ${choiceName}`} // Describes the button for screen readers
     >
       <span className="flex items-center justify-center  md:w-[120px]  w-[80px] rounded-full aspect-square bg-white  shadow-lg">
         <Svg />
